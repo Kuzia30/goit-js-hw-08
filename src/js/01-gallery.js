0b99d105dfd45016f9ccaef26984916a69d5bf2a
Kuzia30 <maxzimka37@gmail.com>
2021-10-27 19:48:17 +0300

    finish task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+const galeryWrap = document.querySelector(".gallery");
+
+const murkup = galleryItems.map(createElement).join('');
+
+function createElement({preview, original, description}) {
+return `<a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`
+}
+
+galeryWrap.insertAdjacentHTML("beforeend", murkup);
+
+galeryWrap.addEventListener('click', onPreventDefault);
+
+function onPreventDefault(event) {
+    event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+lightbox.on('show.simplelightbox', () => {
+    lightbox.options.captionsData = 'alt';
+    lightbox.options.captionDelay = '250';
+})
