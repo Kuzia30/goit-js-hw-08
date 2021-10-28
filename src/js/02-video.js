a9f363632bb6b25a402c5f4a69fb7d2c2cfdec85
Kuzia30 <maxzimka37@gmail.com>
2021-10-28 22:09:10 +0300

    finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
 const player = new Player('vimeo-player');
-const currentTime = localStorage.getItem("time");
-console.log(currentTime);
+addCurrentTime()
+
+    player.on('timeupdate', throttle(function(event) {
+        localStorage.setItem("videoplayer-current-time", event.seconds);
+    }, 1000));
 
 
-
-
-    player.on('timeupdate', function(event) {
-        localStorage.setItem("time", event.seconds);
-    });
+    function addCurrentTime() {
+const currentTime = localStorage.getItem("videoplayer-current-time");
+    if (currentTime) {
+        player.setCurrentTime( currentTime)
+    }
+}
