54c06d5cc876b8fc8d4c5d619aed1effdd28b3d6
Kuzia30 <maxzimka37@gmail.com>
2021-10-28 19:34:56 +0300

    start task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const currentTime = localStorage.getItem("time");
+console.log(currentTime);
+
+
+
+
+    player.on('timeupdate', function(event) {
+        localStorage.setItem("time", event.seconds);
+    });
